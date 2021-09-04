8ccb0f1d9be13ebe80ed2af6135febc6df59aa9c
MolarFox <molar@molarfox.io>
2021-09-04 19:03:23 +1000

    Layout refactor

--- src/website-config.ts
+++ src/website-config.ts
@@ -64,7 +64,7 @@ const config: WebsiteConfig = {
   github: 'https://github.com/MolarFox',
   youtube: 'https://www.youtube.com/channel/UC0_Z_l0ae4x1sntIgAeMepA',
   twitter: 'https://twitter.com/MolarFox',
-  showSubscribe: true,
+  showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
