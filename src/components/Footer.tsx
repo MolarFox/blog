a482760a47e4fb89f16d94caf543468d62e0ba37
MolarFox <molar@molarfox.io>
2021-09-19 17:05:38 +1000

    Remove facebook everywhere, add improved svg icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,11 +21,6 @@ export const Footer: React.FC = () => (
       </section>
       <SiteFooterNav>
         <Link to="/">Latest Posts</Link>
-        {config.facebook && (
-          <a href={config.facebook} target="_blank" rel="noopener noreferrer">
-            Facebook
-          </a>
-        )}
         {config.twitter && (
           <a href={config.twitter} target="_blank" rel="noopener noreferrer">
             Twitter
