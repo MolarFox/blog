ce18fc5177aaa6f2c84a3e9dbcac0a46ef05cc3e
MolarFox <molar@molarfox.io>
2021-09-26 00:30:09 +1000

    Add Resolve light font and use for posts

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,10 +1,14 @@
 import { css } from '@emotion/react';
 
 import ViceCitySans_font from "../fonts/ViceCitySans.woff2";
+
 import ResolveRegular_font from "../fonts/Resolve-Regular.woff2";
 import ResolveBold_font from "../fonts/Resolve-Bold.woff2";
 import ResolveItalic_font from "../fonts/Resolve-RegularIta.woff2";
 import ResolveBoldItalic_font from "../fonts/Resolve-BoldIta.woff2";
+
+import ResolveLight_font from "../fonts/Resolve-Light.woff2";
+import ResolveLightItalic_font from "../fonts/Resolve-LightIta.woff2";
 
 export const ViceCitySans = css`
   @font-face {
@@ -34,6 +38,32 @@ export const Resolve = css`
   @font-face {
     font-family: "Resolve";
     src: url("${ResolveBoldItalic_font}") format("woff2");
+    font-weight: bold;
+    font-style: italic;
+  }
+`;
+
+export const ResolveLight = css`
+  @font-face {
+    font-family: "ResolveLight";
+    src: url("${ResolveLight_font}") format("woff2");
+  }
+
+  @font-face {
+    font-family: "ResolveLight";
+    src: url("${ResolveRegular_font}") format("woff2");
+    font-weight: bold;
+  }
+
+  @font-face {
+    font-family: "ResolveLight";
+    src: url("${ResolveLightItalic_font}") format("woff2");
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: "ResolveLight";
+    src: url("${ResolveItalic_font}") format("woff2");
     font-weight: bold;
     font-style: italic;
   }
